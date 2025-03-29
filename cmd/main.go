0c4104a9135e7742975137c0398c4ff2a9cdbe59
KamigamiNoGigan <KanzArtem11@mail.ru>
2025-03-30 00:28:38 +0300

    openai

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"tg-bot-rss/internal/botkit"
+	"tg-bot-rss/internal/config"
+	"tg-bot-rss/internal/fetcher"
+	"tg-bot-rss/internal/notifier"
+	"tg-bot-rss/internal/storage"
+	"tg-bot-rss/internal/summary"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	botAPI, err := tgbotapi.NewBotAPI(config.Get().TelegramBotToken)
+	if err != nil {
+		log.Printf("failed to create bot: %v", err)
+		return
+	}
+
+	db, err := sqlx.Connect("postgres", config.Get().DatabaseDSN)
+	if err != nil {
+		log.Printf("failed to connect to database: %v", err)
+		return
+	}
+	defer db.Close()
+
+	var (
+		articleStorage = storage.NewArticleStorage(db)
+		sourceStorage  = storage.NewSourceStorage(db)
+		fetcher        = fetcher.New(
+			articleStorage,
+			sourceStorage,
+			config.Get().FetchInterval,
+			config.Get().FilterKeywords,
+		)
+		notifier = notifier.New(
+			articleStorage,
+			summary.NewOpenAISummarizer(config.Get().OpenAIKey, config.Get().OpenAIPrompt),
+			botAPI,
+			config.Get().NotificationInterval,
+			2*config.Get().FetchInterval,
+			config.Get().TelegramChannelID,
+		)
+	)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	newsBot := botkit.New(botAPI)
+	newsBot.RegisterCmdView("start", botkit.ViewCmdStart())
+
+	go func(ctx context.Context) {
+		if err := fetcher.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("failed to start fetcher: %v", err)
+				return
+			}
+			log.Println("fetcher stopped")
+		}
+	}(ctx)
+
+	go func(ctx context.Context) {
+		if err := notifier.Start(ctx); err != nil {
+			if !errors.Is(err, context.Canceled) {
+				log.Printf("failed to start notifier: %v", err)
+				return
+			}
+			log.Println("notifier stopped")
+		}
+	}(ctx)
+
+	if err := newsBot.Run(ctx); err != nil {
+		if !errors.Is(err, context.Canceled) {
+			log.Printf("failed to start bot: %v", err)
+			return
+		}
+		log.Println("bot stopped")
+	}
 }
